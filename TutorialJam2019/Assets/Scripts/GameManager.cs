3667d7db6063bcf9383de9b51dea6d49dde3db65
JackHill-SJ <hilljack1856@gmail.com>
2019-09-21 19:18:17 -0400

    Added Damage Punch Hitbox

---
+++ TutorialJam2019/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
+
+    public int P1Life;
+    public int P2Life;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(P1Life <= 0)
+        {
+            player1.SetActive(false);
+        }
+
+        if (P2Life <= 0)
+        {
+            player2.SetActive(false);
+        }
+    }
+
+    public void HurtP1()
+    {
+        P1Life -= 1;
+    }
+
+    public void HurtP2()
+    {
+        P2Life -= 1;
+    }
+}
---
+++ TutorialJam2019/Assets/Scripts/PunchHitbox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchHitbox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "player1")
+        {
+            FindObjectOfType<GameManager>().HurtP1();
+        }
+
+        if(other.tag == "player2")
+        {
+            FindObjectOfType<GameManager>().HurtP2();
+        }
+    }
+}
