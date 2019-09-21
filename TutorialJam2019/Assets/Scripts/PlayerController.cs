145778e97721de79fb4a4fd9570d654dcf81252c
Robert Williams <43484817+robbytaku@users.noreply.github.com>
2019-09-21 18:44:14 -0400

    Finally, Double Controller Support

--- TutorialJam2019/Assets/Scripts/PlayerController.cs
+++ TutorialJam2019/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int controllerNumber;
+
     public float moveSpeed;
     public float jumpForce;
     private float moveInput;
